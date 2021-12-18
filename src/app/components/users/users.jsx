247912da212e56517ab3626843c2fef91f6d398b
Imran-72 <petrow318@gmail.com>
2021-12-18 11:41:32 +0300

    получаю пользователей и отображаю на ui

--- src/app/components/users/users.jsx
+++ src/app/components/users/users.jsx
@@ -1,3 +1,4 @@
+import * as axios from "axios";
 import React, { useState } from "react";
 
 const Users = () => {
@@ -5,20 +6,29 @@ const Users = () => {
 
   console.log("users", users);
   const getUsers = async () => {
-    const response = await fetch(
+    const response = await axios(
       "https://social-network.samuraijs.com/api/1.0/users?page=1&count=10"
     );
-    const json = await response.json();
-    setUsers(json);
+    const data = await response.data;
+    setUsers(data.items);
   };
   return (
     <>
       {users &&
         users.map((item) => {
           return (
-            <ul>
-              <li>{item}</li>
-            </ul>
+            <div style={{ display: "block" }}>
+              <img
+                src={
+                  item.photos.small == null
+                    ? "https://img2.freepng.ru/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg"
+                    : item.photos.small
+                }
+                alt="avatar"
+                width="100"
+              />
+              <p>{item.name}</p>
+            </div>
           );
         })}
       <div>
